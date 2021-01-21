d31c2f2de9fd51f54ed4033c7ffd804d1ff25a65
GustavMaki <gustav.siljamaki@gmail.com>
2021-01-21 14:30:08 +0100

    v2. Compleated Cart function (show/hide entries). Added order function that clears the shoppingcart storage.

---
+++ ProductManagement.Windows.UI/LocalStorage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductManagement.Business.Concrete;
+
+namespace ProductManagement.Windows.UI
+{
+    class LocalStorage : IRepository
+    {
+        //Vi behöver en lokal lagring av produkter
+        private List<Product> products;
+
+        public LocalStorage()
+        {
+            products = new List<Product>();
+
+            //Vi fejkar lite produkter som start
+            products.Add(new Product() { Name = "Kockkniv 30cm", Price = 100 });
+            products.Add(new Product() { Name = "Skalkniv 10cm", Price = 25 });
+            products.Add(new Product() { Name = "Gafflar 10x", Price = 100 });
+            products.Add(new Product() { Name = "Stekpanna", Price = 150 });
+            products.Add(new Product() { Name = "Kastrullset", Price = 100 });
+        }
+        public void Add(Product product)
+        {
+            products.Add(product);
+        }
+
+        public List<Product> GetProducts()
+        {
+            return products;
+        }
+    }
+}
